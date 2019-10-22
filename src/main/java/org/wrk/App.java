3d2bd53fb5bed66e08af2a0ebba2ca43b1046b9d
wrk <w-rkui@qq.com>
2019-10-22 14:38:33 +0800

    第一次提交

--- src/main/java/org/wrk/App.java
+++ src/main/java/org/wrk/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello sssWorld!" );
+        System.out.println( "Hello ssssssWorld!" );
     }
 }
